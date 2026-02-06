a72ea0946689c4fde915d870f58747812833053e
abhijeetpalanki <ajpalanki@gmail.com>
2026-02-05 21:00:38 -0600

    Homepage, Navbar & ResumeCard components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,34 @@
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Thrive" },
+    { name: "description", content: "AI Resume Analyzer Assistant" },
   ];
 }
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard resume={resume} key={resume.id} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
